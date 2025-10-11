1ea8b64ee1221cb980da391beb44c7de7c3b1f7a
Amanuel Agegnehu <amanuelage873@gmail.com>
2025-10-11 21:19:54 +0300

    feat: add layout wrapper, header, footer, and landing page with CTA

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,13 +1,14 @@
+import { ComponentProps } from "@/interfaces";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout: React.FC<ComponentProps> = ({ children }) => {
   return (
-    <div>
+    <>
       <Header />
       <main className="min-h-screen">{children}</main>
       <Footer />
-    </div>
+    </>
   );
 };
 
